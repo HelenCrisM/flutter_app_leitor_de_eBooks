8c788cb3d2242355373446aab743c81ec7c2bbda
Helen Cris <helencristavares@gmail.com>
2023-11-24 23:07:06 -0300

    🦄 refactor: refactor model

--- lib/models/book_model.dart
+++ lib/models/book_model.dart
@@ -1,13 +1,10 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:convert';
-
 class BookModel {
-  final int id;
-  final String title;
-  final String author;
-  final String coverUrl;
-  final String downloadUrl;
-  final bool isFavorite;
+  int id;
+  String title;
+  String author;
+  String coverUrl;
+  String downloadUrl;
+  bool? isFavorite;
 
   BookModel({
     required this.id,
@@ -18,75 +15,21 @@ class BookModel {
     required this.isFavorite,
   });
 
-  BookModel copyWith({
-    int? id,
-    String? title,
-    String? author,
-    String? coverUrl,
-    String? downloadUrl,
-    bool? isFavorite,
-  }) {
-    return BookModel(
-      id: id ?? this.id,
-      title: title ?? this.title,
-      author: author ?? this.author,
-      coverUrl: coverUrl ?? this.coverUrl,
-      downloadUrl: downloadUrl ?? this.downloadUrl,
-      isFavorite: isFavorite ?? this.isFavorite,
-    );
-  }
+  factory BookModel.fromJson(Map<String, dynamic> json) => BookModel(
+        id: json["id"],
+        title: json["title"],
+        author: json["author"],
+        coverUrl: json["cover_url"],
+        downloadUrl: json["download_url"],
+        isFavorite: json["is_favorite"],
+      );
 
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'id': id,
-      'title': title,
-      'author': author,
-      'coverUrl': coverUrl,
-      'downloadUrl': downloadUrl,
-      'isFavorite': isFavorite,
-    };
-  }
-
-  factory BookModel.fromMap(Map<String, dynamic> map) {
-    return BookModel(
-      id: map['id'] as int,
-      title: map['title'] as String,
-      author: map['author'] as String,
-      coverUrl: map['coverUrl'] as String,
-      downloadUrl: map['downloadUrl'] as String,
-      isFavorite: map['isFavorite'] as bool,
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory BookModel.fromJson(String source) =>
-      BookModel.fromMap(json.decode(source) as Map<String, dynamic>);
-
-  @override
-  String toString() {
-    return 'BookModel(id: $id, title: $title, author: $author, coverUrl: $coverUrl, downloadUrl: $downloadUrl, isFavorite: $isFavorite)';
-  }
-
-  @override
-  bool operator ==(covariant BookModel other) {
-    if (identical(this, other)) return true;
-
-    return other.id == id &&
-        other.title == title &&
-        other.author == author &&
-        other.coverUrl == coverUrl &&
-        other.downloadUrl == downloadUrl &&
-        other.isFavorite == isFavorite;
-  }
-
-  @override
-  int get hashCode {
-    return id.hashCode ^
-        title.hashCode ^
-        author.hashCode ^
-        coverUrl.hashCode ^
-        downloadUrl.hashCode ^
-        isFavorite.hashCode;
-  }
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "author": author,
+        "cover_url": coverUrl,
+        "download_url": downloadUrl,
+        "is_favorite": isFavorite,
+      };
 }
