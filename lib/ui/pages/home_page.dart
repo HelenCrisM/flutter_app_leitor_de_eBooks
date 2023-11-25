5764b189f7c434a5d5c493bda36d601bfa57139a
Helen Cris <helencristavares@gmail.com>
2023-11-24 23:02:31 -0300

    🦄 refactor: add card widget

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,21 +1,23 @@
+import 'package:app_leitor_de_ebooks/services/book_service.dart';
+import 'package:app_leitor_de_ebooks/ui/widgets/display_card_widget.dart';
+import 'package:app_leitor_de_ebooks/ui/widgets/header_title_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
-
-  final String title;
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
+  BookService service = Get.put(BookService());
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    service.getBook();
+    super.initState();
   }
 
   @override
@@ -23,26 +25,20 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const HeaderTitleWidget(text: 'Today'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+      body: SingleChildScrollView(
+          physics: const ScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: displayCardWidget(service.listModel)
+            
+          )),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.pushNamed(context, 'favorite');
+        },
+        child: const Icon(Icons.star),
       ),
     );
   }
