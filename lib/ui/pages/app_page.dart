c1ec7994332ff096e988d368afff6901be9f2309
Helen Cris <helencristavares@gmail.com>
2023-11-24 23:08:32 -0300

    🦄 refactor: add routes and get initialization

--- lib/ui/pages/app_page.dart
+++ lib/ui/pages/app_page.dart
@@ -1,19 +1,22 @@
+import 'package:app_leitor_de_ebooks/routes/app_routes.dart';
 import 'package:app_leitor_de_ebooks/ui/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      onGenerateRoute: AppRoutes.onGenerateRoute,
+      home: const HomePage(),
     );
   }
 }
