e40759e8f76ac61c1ad68ff60414e4bf4c7ec492
Helen Cris <helencristavares@gmail.com>
2023-11-25 06:54:10 -0300

    ✨ feat: add openBook implementation

--- lib/ui/widgets/card_widget.dart
+++ lib/ui/widgets/card_widget.dart
@@ -1,6 +1,13 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:app_leitor_de_ebooks/controller/card_controller.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:vocsy_epub_viewer/epub_viewer.dart';
 
 // ignore: must_be_immutable
@@ -26,10 +33,73 @@ class CardWidget extends StatefulWidget {
 
 class _CardWidgetState extends State<CardWidget> {
   CardController controller = Get.put(CardController());
+  Dio dio = Dio();
+  String filePath = "";
+
+  @override
+  void initState() {
+    download();
+    super.initState();
+  }
+
+  startDownload() async {
+    Directory? appDocDir = Platform.isAndroid
+        ? await getExternalStorageDirectory()
+        : await getApplicationDocumentsDirectory();
+
+    String path = '${appDocDir!.path}/${widget.title}.epub';
+    File file = File(path);
+
+    if (!File(path).existsSync()) {
+      await file.create();
+      await dio
+          .download(
+        widget.downloadUrlImage,
+        path,
+        deleteOnError: true,
+        onReceiveProgress: (receivedBytes, totalBytes) {},
+      )
+          .whenComplete(() {
+        setState(() {
+          filePath = path;
+        });
+      });
+    } else {
+      setState(() {
+        filePath = path;
+      });
+    }
+  }
+
+  /// ANDROID VERSION
+  Future<void> fetchAndroidVersion() async {
+    final PermissionStatus status = await Permission.storage.request();
+    if (status == PermissionStatus.granted) {
+      await startDownload();
+    } else {
+      await Permission.storage.request();
+    }
+  }
+
+  download() async {
+    if (Platform.isIOS) {
+      final PermissionStatus status = await Permission.storage.request();
+      if (status == PermissionStatus.granted) {
+        await startDownload();
+      } else {
+        await Permission.storage.request();
+      }
+    } else if (Platform.isAndroid) {
+      await fetchAndroidVersion();
+    } else {
+      PlatformException(code: '500');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return InkWell(
+      child: Container(
         decoration: BoxDecoration(
             color: const Color.fromARGB(255, 207, 180, 233),
             borderRadius: BorderRadius.circular(15)),
@@ -39,29 +109,29 @@ class _CardWidgetState extends State<CardWidget> {
           children: [
             Center(
               child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        InkWell(
-                          onTap: () => controller.downloadBook(
-                               widget.downloadUrlImage, widget.title),
-                          child: Image.network(
-                            widget.coverImage,
-                          ),
-                        ),
-                        Text(
-                          widget.title,
-                          style: const TextStyle(
-                              color: Color(0xff7b2cbf), fontSize: 16),
-                        ),
-                        const SizedBox(height: 3),
-                        Text(
-                          widget.author,
-                          style: const TextStyle(
-                            color: Color(0xffd4d4d4),
-                          ),
-                        )
-                      ],
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  InkWell(
+                    onTap: () => controller.downloadBook(
+                        widget.downloadUrlImage, widget.title),
+                    child: Image.network(
+                      widget.coverImage,
                     ),
+                  ),
+                  Text(
+                    widget.title,
+                    style:
+                        const TextStyle(color: Color(0xff7b2cbf), fontSize: 16),
+                  ),
+                  const SizedBox(height: 3),
+                  Text(
+                    widget.author,
+                    style: const TextStyle(
+                      color: Color(0xffd4d4d4),
+                    ),
+                  )
+                ],
+              ),
             ),
             Align(
               alignment: Alignment.topRight,
@@ -75,7 +145,46 @@ class _CardWidgetState extends State<CardWidget> {
               ),
             ),
           ],
-        ),    
+        ),
+      ),
+      onTap: () {
+        log("=====filePath======${filePath.toString()}");
+        if (filePath == "") {
+          download();
+        } else {
+          download();
+          openBook();
+          setState(() {
+            filePath == '';
+          });
+        }
+      },
+    );
+  }
+
+  openBook() {
+    VocsyEpub.setConfig(
+      themeColor: Theme.of(context).primaryColor,
+      identifier: "iosBook",
+      scrollDirection: EpubScrollDirection.ALLDIRECTIONS,
+      allowSharing: true,
+      enableTts: true,
+      nightMode: true,
+    );
+
+    // get current locator
+    VocsyEpub.locatorStream.listen((locator) {
+      log('LOCATOR: $locator');
+    });
+
+    VocsyEpub.open(
+      filePath,
+      lastLocation: EpubLocator.fromJson({
+        "bookId": "2239",
+        "href": "/OEBPS/ch06.xhtml",
+        "created": 1539934158390,
+        "locations": {"cfi": "epubcfi(/0!/4/4[simple_book]/2/2/6)"}
+      }),
     );
   }
 }
